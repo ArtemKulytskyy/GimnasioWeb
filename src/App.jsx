557c851572eefa5d8433c0f7f7135612a6144922
Izani Achega <iachegavi24dw@ikzubirimanteo.com>
2025-12-15 11:01:27 +0100

    jp sube pagina de inicio de sesion

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Locales from './components/Locales'
 import Clases from './components/Clases'
 import ScrollToTop from './components/common/ScrollToTop'
 import Entrenadores from './components/Entrenadores'
+import IniciarSesionForm from './components/IniciarSesionForm'
 //import './App.css'
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path="/locales" element={<Locales />}/>
           <Route path="/clases" element={<Clases />}/>
           <Route path="/entrenadores" element={<Entrenadores />}/>
+          <Route path="/login" element={<IniciarSesionForm />}/>
         </Routes>
       </BrowserRouter>
     </>
